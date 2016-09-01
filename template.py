0b823ce69e968cb4199df3b3085e7033d43a9335
Dipit Malhotra <dipitmalhotra@gmail.com>
2016-09-01 13:35:22 -0500

    NLTK template

---
+++ template.py
@@ -0,0 +1,31 @@
+# -- coding: UTF-8 --
+import nltk
+from nameparser.parser import HumanName
+
+def get_human_names(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+    person_list = []
+    person = []
+    name = ""
+    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+        for leaf in subtree.leaves():
+            person.append(leaf[0])
+        if len(person) > 1: #avoid grabbing lone surnames
+            for part in person:
+                name += part + ' '
+            if name[:-1] not in person_list:
+                person_list.append(name[:-1])
+            name = ''
+        person = []
+
+    return (person_list)
+
+
+
+names = get_human_names("Google was founded by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University, California. Together, they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a privately held company on September 4, 1998. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its new headquarters in Mountain View, California, nicknamed the Googleplex.[10]")
+print "LAST, FIRST"
+for name in names:
+    last_first = HumanName(name).last + ', ' + HumanName(name).first
+    print last_first
