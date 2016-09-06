ef812b2e1aaecd8fa696d487c45aea9b868682bc
Dipit Malhotra <dipitmalhotra@gmail.com>
2016-09-06 14:54:42 -0500

    Updated with templates

--- pdf.py
+++ pdf.py
@@ -1,11 +1,15 @@
+# -- coding: UTF-8 --
+import nltk
+from nameparser.parser import HumanName
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
 #!/usr/bin/python
-import MySQLdb
+# import MySQLdb
 import sqlite3
+import re
 
 import os
 def convert_pdf_to_txt(path):
@@ -31,18 +35,72 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
+def get_human_names(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+    person_list = []
+    person = []
+    name = ""
+    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+        for leaf in subtree.leaves():
+            person.append(leaf[0])
+        if len(person) > 1: #avoid grabbing lone surnames
+            for part in person:
+                name += part + ' '
+            if name[:-1] not in person_list:
+                person_list.append(name[:-1])
+            name = ''
+        person = []
+
+    return (person_list)
+
+test= {}
+test1={}
+def quotes_name(r):
+    quoted1 = re.compile('(.+)"[^"]+" ')
+    quoted = re.compile('(.+)"[^"]+",')
+    quoted2 = re.compile('(.+)"[^"]+".')
+
+    for value in (quoted.findall(r) or quoted1.findall(r) or (quoted2.findall(r))):
+        test1['name'] = value
+        print test1
+
+def quotes(x):
+    quoted = re.compile('"[^"]*"')
+    for value in (quoted.findall(x)):
+        l= len(value)
+        if l> 50:
+            test['title']= value
+            print test
+
+
 def extract():
-    string = convert_pdf_to_txt("/Users/dipit/Documents/RA/RA/sample.pdf")
-    lines = list(filter(bool, string.split('.')))
-    Data = {}
-    for i in range(len(lines)):
-        if 'References' in lines[i]:
-            Data = (lines[i + 1])
-            d = Data.split('  ')
-            for r in range(4):
-                x= "INSERT INTO Reference (NAME) \
-                   VALUES" + '(' + '"{}"'.format(d[r]) + ')'
-                data(x)
+    string = convert_pdf_to_txt("/Users/dipit/Documents/RA/RA/test.pdf")
+    # print string
+    quotes(string)
+    # print "\n"
+    quotes_name(string)
+
+
+
+    # lines = list(filter(bool, string.split('"')))
+    # Data = {}
+    # for i in range(len(lines)):
+    #     if 'References' in lines[i]:
+    #         Data = (lines[i + 1])
+    #         d = Data.split('  ')
+    #         names = get_human_names(str(d))
+    #         print d
+    #         print "LAST, FIRST"
+    #         for name in names:
+    #             last_first = HumanName(name).last + ', ' + HumanName(name).first
+    #             print last_first
+    #
+    #         for r in range(4):
+    #             x= "INSERT INTO Reference (NAME) \
+    #                VALUES" + '(' + '"{}"'.format(d[r]) + ')'
+    #             data(x)
 
 
 def data(sql):
@@ -63,7 +121,7 @@ def data(sql):
     conn.close()
 
 if __name__ == "__main__":
-    hello_world_text = convert_pdf_to_txt("/Users/dipit/Documents/RA/RA/sample.pdf")
+    hello_world_text = convert_pdf_to_txt("/Users/dipit/Documents/RA/RA/test.pdf")
     extract()
     text_file = open("Output.txt", "w")
     text_file.write(hello_world_text)
